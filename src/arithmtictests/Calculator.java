2746be71366828028cd8389b45c533738599b7c3
sp16-bse-075 <sp16-bse-075@169.254.60.121>
2019-07-01 16:25:07 +0500

    This is my first version of the project. Thanks Allah

---
+++ src/arithmtictests/Calculator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmtictests;
+
+/**
+ *
+ * @author sp16-bse-075
+ */
+public class Calculator 
+{
+
+  
+    public int add(int a , int b)
+    {
+        return a+b;
+        
+    }
+     public int sub(int a , int b)
+    {
+        return a-b;
+    }
+      public int mul(int a , int b)
+    {
+        return a*b;
+    }
+      
+       public int div(int a , int b)
+    {
+        return a/b;
+    }
+    
+    
+}
---
+++ test/arithmtictests/CalculatorTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmtictests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sp16-bse-075
+ */
+public class CalculatorTest {
+    
+    public CalculatorTest() 
+    {
+       
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+        
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown() 
+    {
+    }
+
+    @Test
+    public void testSomeMethod() 
+    {
+       
+    }
+    @Test
+    public void add()
+    {
+        Calculator obj= new Calculator();
+        assertEquals( 100, obj.add(87,13)  );
+       
+    }
+      @Test
+    public void sub()
+    {
+        Calculator obj= new Calculator();
+        assertNotEquals( 4, obj.sub(87,13)  );
+       
+    }
+      @Test
+    public void mul()
+    {
+        Calculator obj= new Calculator();
+         assertEquals( 50, obj.mul(10,5)  );
+       
+    }
+      @Test
+    public void div()
+    {
+        Calculator obj= new Calculator();
+        assertEquals( 2, obj.div(10,5)  );
+       
+    }
+    
+}
